61f2dd9dbc5b6280c732585bad5b600d9485813d
Dawson Claybrook <joystickclay@gmail.com>
2023-07-27 19:55:14 -0600

    restructured sudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,47 @@ var setting = document.querySelector("#setting")
 var submitButton = document.querySelector("#mainSubmit")
 var cardHolder = document.querySelector("#cardHolder")
 
+
+//function createCard(requestURL)
+
+//         var createCard = document.createElement("div")
+//          createCard.className = "card"
+
+//          var cardInfo = document.createElement("section")
+//          cardInfo.className = "activityInfo"
+//          createCard.appendChild(cardInfo)
+
+//          Create h2 for activity name, p for description, h3 for cost
+
+//          var cardMap = document.createElement("section")
+//          cardMap.className = "mapVisual"
+//          createCard.appendChild(cardMap)
+
+//          create h3 for location name, img for map image, and p for any extra info
+
+//          cardHolder.appendChild(createCard)
+//}
+
+function getBoredURL(){
+    var requestURL = "URL" + "input values"
+    // fetch(requestURL)
+    //      .then(function(response){
+    //          return JSON.parse(response)
+    //      })
+    //      .then(function(data){
+    //          var activityObject = data
+    //          return activityObject
+    //      })
+
+    //get maplocation and search up the activity on the map api in relation to location.
+    //check if the locations shown are within the specified range.
+    
+    //if(locationsInRange == 0){
+    //      getBoredURL()    
+    //}
+
+}
+
 function submitForm(){
     //get value from all form inputs
 
@@ -18,30 +59,9 @@ function submitForm(){
     var cardCountNum = 5
 
     for(i = 0; i < cardCountNum; i++){
-        var requestURL = "URL" + "input values"
+        //getBoredURL()
 
-        //fetch(requestURL)
-        //      .then (function(response){
-        //             return JSON.parse(response)    
-    //        })
-    //       .then (function(data){
-        //         var createCard = document.createElement("div")
-        //          createCard.className = "card"
-                    
-        //          var cardInfo = document.createElement("section")
-        //          cardInfo.className = "activityInfo"
-        //          createCard.appendChild(cardInfo)
-
-        //          Create h2 for activity name, p for description, h3 for cost
-
-        //          var cardMap = document.createElement("section")
-        //          cardMap.className = "mapVisual"
-        //          createCard.appendChild(cardMap)
-
-        //          create h3 for location name, img for map image, and p for any extra info
-
-        //          cardHolder.appendChild(createCard)
-    //})
+        // createCard()
     }
     
 }   
