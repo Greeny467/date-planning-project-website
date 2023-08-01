c84c8b7bbd85d9a107ba1e817c53195b256da90c
Dawson Claybrook <joystickclay@gmail.com>
2023-07-31 19:38:11 -0600

    added 50-99 filter items

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,26 +41,222 @@ function getBoredURL(category, price){
 
 
             //Response filter, due to limited api versatility
-            if (activityObject.activity == "Learn Express.js"){
+            if (activityObject.key == "3943506"){
                 var atHome = "yes"
                 var twoPerson = "yes"
                 
             }
-            if (activityObject.activity == "Learn to greet someone in a new language"){
+            else if (activityObject.key == "4704256"){
                 var atHome = "yes"
                 var twoPerson = "yes"
             }
-            if (activityObject.activity == "Learn how to play a new sport"){
+            else if (activityObject.key == "5808228"){
                 var atHome = "no"
                 var twoPerson = "yes"
             }
-            if (activityObject.activity == "Text a friend you haven't talked to in a long time"){
+            else if (activityObject.key == "6081071"){
                 var atHome = "yes"
                 var twoPerson = "no"
             }
             // 5-49
 
             //50-99
+            else if (activityObject.key == "9364041"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "1162360"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "8364626"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "1934228"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "9026787"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "6596257"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "3141417"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "4124860"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "8557562"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "4565537"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "1718657"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "8503795"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "2352669"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "4894697"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "3456114"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "4101229"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "2896176"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "6301585"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "8321894"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "6808057"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "7023703"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "8731971"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "8926492"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "2790297"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "1645485"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "4809815"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "4379552"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "6778219"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "7091374"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "4387026"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "6693574"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "8344539"){
+                var atHome = "no"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "4558850"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "8253550"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "5585829"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "9072906"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "4877086"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "1288934"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "8832605"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "6613428"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "3818400"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "8779876"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "6482790"){
+                var atHome = "no"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "2735499"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "5881647"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "9924423"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "9366464"){
+                var atHome = "yes"
+                var twoPerson = "no"
+            }
+            else if (activityObject.key == "8081693"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
+            else if (activityObject.key == "5554727"){
+                var atHome = "yes"
+                var twoPerson = "yes"
+            }
 
             //100-149
 
